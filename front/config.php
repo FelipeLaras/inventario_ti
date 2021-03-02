d8ba2971646581e439b5cfee23451df99e15de54
Felipe Lara <69820156+FelipeLaras@users.noreply.github.com>
2021-03-02 09:05:28 -0300

    new tela
    informações banco de dados

--- front/config.php
+++ front/config.php
@@ -3,7 +3,7 @@ require_once('header.php');
 require_once('../bd/conexao.php');
 
 //permissão de tela
-if($_SESSION['perfil'] != '0'){
+if ($_SESSION['perfil'] != '0') {
   exit;
 }
 
@@ -63,7 +63,26 @@ $usuario = $resultUsuarios->fetch_assoc();
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-4 col-md-6 mb-4">
+    <div class="col-xl-3 col-md-6 mb-4">
+      <a href='infodb.php?pagina=2' class="text-decoration">
+        <div class="card border-left-danger shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="menu font-weight-bold text-danger text-uppercase mb-1">Informação</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">Banco de dados</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-database fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
       <a href='perfil.php?pagina=2' class="text-decoration">
         <div class="card border-left-warning shadow h-100 py-2">
           <div class="card-body">
@@ -73,7 +92,7 @@ $usuario = $resultUsuarios->fetch_assoc();
                 <div class="h5 mb-0 font-weight-bold text-gray-800">Editar o meu perfil</div>
               </div>
               <div class="col-auto">
-                <i class="fas fa-user fa-2x text-gray-300"></i>
+                <i class="fas fa-user-edit fa-2x text-gray-300"></i>
               </div>
             </div>
           </div>
